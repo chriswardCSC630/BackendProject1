397ad96bd68b7f1e2018e4fd3fd16d3aa7c69d81
Chris <cward@andover.edu>
2019-04-01 22:57:19 -0400

    database commit

--- project_database/urls.py
+++ project_database/urls.py
@@ -1,4 +1,4 @@
-"""project_1 URL Configuration
+"""project_database URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
